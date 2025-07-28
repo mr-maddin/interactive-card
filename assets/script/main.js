823594f421619b098b6b4ea6639d294cc9c6510a
Maddin <martin.weisskircher@gmail.com>
2025-07-28 21:29:57 +0200

    Javascript Eingabe programmiert

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,34 @@
 "use strict";
+
+let nameInput = document.getElementById("cardholder-input");
+let nameOutput = document.getElementById("cardholder-output");
+
+let cardInput = document.getElementById("cardnumber-input");
+let cardOutput = document.getElementById("cardnumber-output");
+
+let expDateInputMM = document.getElementById("exp-mm");
+let expDateInputYY = document.getElementById("exp-yy");
+let expDateOutput = document.getElementById("expire-date-output");
+
+let cvcInput = document.getElementById("cvc-input");
+let cvcOutput = document.getElementById("cvc-output");
+
+nameInput.addEventListener("input", function () {
+  nameOutput.textContent = nameInput.value || "John Doe";
+});
+
+cardInput.addEventListener("input", function () {
+  cardOutput.textContent = cardInput.value || "0000 0000 0000 0000";
+});
+
+expDateInputMM.addEventListener("input", function () {
+  expDateOutput.textContent = expDateInputMM.value || "00" + "/" + expDateInputYY.value || "00";
+});
+
+expDateInputYY.addEventListener("input", function () {
+  expDateOutput.textContent = expDateInputMM.value || "00" + "/" + expDateInputYY.value || "00";
+});
+
+cvcInput.addEventListener("input", function () {
+  cvcOutput.textContent = cvcInput.value | "000";
+});
